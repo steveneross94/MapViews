3dba96a30c2d03d7be153c4695bb911667771180
steveneross94 <ross.steven@me.com>
2020-09-29 23:37:06 -0400

    geolocation working

--- App.js
+++ App.js
@@ -2,13 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Geolocation from '@react-native-community/geolocation';
+import FetchLocation from './components/FetchLocation'
+
+export default class App extends React.Component {
+  getUserLocation = () => {
+    console.log('IT WORKED THATS SO FIRE')
+    Geolocation.getCurrentPosition(position => {
+      console.log(position), err => console.log(err)
+    })
+  }
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation={this.getUserLocation}/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
